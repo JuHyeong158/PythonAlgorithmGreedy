7c62d4c6522fa6c41ba1991b00bb1f90d50b81f8
JuHyeong158 <110215982+JuHyeong158@users.noreply.github.com>
2022-09-20 12:08:15 +0900

    Add files via upload

---
+++ card game.py
@@ -0,0 +1,9 @@
+print("수를 입력하세요 : ")
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+print(result)
